68d740f04a6f9c1d1254f112ab300110e2a84f34
s23-rp <s23-rp@it-step.edu>
2016-10-05 11:35:17 +0300

    add gridview for products

--- ITStoreClient/AdminPanel.xaml.cs
+++ ITStoreClient/AdminPanel.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows;
 using System.Windows.Media;
 using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace ITStoreClient
 {
@@ -10,52 +12,44 @@ namespace ITStoreClient
 	/// </summary>
 	public partial class AdminPanel : Window
 	{
-		SqlConnection connection;
-		SqlConnectionStringBuilder builder;
+        private ObservableCollection<Product> products;
 
-		SqlCommand cmd;
-		SqlDataReader reader;
 
-		string databaseLogin = "sa"; // default database superadmin name
-		string databasePassword = "1"; // default password
-
-		public AdminPanel()
+        public AdminPanel()
 		{
 			InitializeComponent();
-			//ConnectToDatabase();
+			
 		}
 
-		private void ConnectToDatabase()
-		{
-			try {
-				builder = new SqlConnectionStringBuilder();
-				builder.UserID = databaseLogin;
-				builder.Password = databasePassword;
-				builder.InitialCatalog = "ShopDar";
-				builder.DataSource = @".\SQLEXPRESS";
+				
+        public static ObservableCollection<Product> GetProducts()
+        {
+            ShopEntities context = new ShopEntities();
 
-				connection = new SqlConnection(builder.ConnectionString);
-				connection.Open();
-			}
-			catch (Exception ex)
-			{
-				status.Foreground = Brushes.Red;
-				status.Content = ex.Message;
-				status.Foreground = Brushes.Black;
-			}
-		}
+            return new ObservableCollection<Product>(
+                    context.Products.Select(p => p).ToList<Product>());
+        }
 
-		private void LoadTables()
-		{
-			//cmd = connection.CreateCommand();
-			//cmd.CommandText = @"SELECT * FROM sys.tables;";
-			//reader = cmd.ExecuteReader();
+       
 
-			//while (reader.Read())
-			//{
-			//	comboBoxTables.Items.Add(reader.GetString(reader.GetOrdinal("name")));
-			//}
-			//reader.Close();
-		}
-	}
+        private void cmdGetProduct_Click(object sender, RoutedEventArgs e)
+        {
+            products = GetProducts();
+            lstCars.ItemsSource = products;
+        }
+
+        private void cmdDeleteProduct_Click(object sender, RoutedEventArgs e)
+        {
+            //cars.Remove((CarTable)lstCars.SelectedItem);
+        }
+        private void cmdAddCar_Click(object sender, RoutedEventArgs e)
+        {
+            //cars.Remove((CarTable)lstCars.SelectedItem);
+        }
+        private void cmdDeleteCar_Click(object sender, RoutedEventArgs e)
+        {
+            //cars.Remove((CarTable)lstCars.SelectedItem);
+        }
+
+    }
 }
--- ITStoreClient/Category.cs
+++ ITStoreClient/Category.cs
@@ -25,5 +25,9 @@ namespace ITStoreClient
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Product> Products { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
