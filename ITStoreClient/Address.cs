7b81334f1f43f6ceaa059659d3076754373fbb01
gaz84 <nazariy84@gmail.com>
2016-10-23 14:40:38 +0300

    fixed bags in admin panel

--- ITStoreClient/Address.cs
+++ ITStoreClient/Address.cs
@@ -11,7 +11,7 @@ namespace ITStoreClient
 {
     using System;
     using System.Collections.Generic;
-    using System.Text;
+    
     public partial class Address
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -30,10 +30,11 @@ namespace ITStoreClient
         public virtual ICollection<Producer> Producers { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Supplier> Suppliers { get; set; }
+
         public override string ToString()
         {
             return Country + "  " + Town + "  " + Street;
-            
+
         }
     }
 }
--- ITStoreClient/Category.cs
+++ ITStoreClient/Category.cs
@@ -25,6 +25,7 @@ namespace ITStoreClient
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Product> Products { get; set; }
+
         public override string ToString()
         {
             return Name;
